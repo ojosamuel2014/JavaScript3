aae4d376e05f04ea74ac9ef7cd97b577cf3c5013
ojosamuel14 <ojosamuel2014@gmail.com>
2020-04-27 14:10:37 +0100

    update

--- JavaScript3.js
+++ JavaScript3.js
@@ -1,3 +1,48 @@
+// declear a variable to an empty array
+let myArray = [];
+// Checking if the argument is equal to the conditions
+function myFunction(num) {
+    // for loop appending num to x  
+    for (x = 1; x <= num; x++) {
+        // the switch statement is checking if the loops is equal to the cases iterate else return the value of x".
+        switch (true) {
+            case (x % 2 == 0 && x % 3 == 0 && x % 5 == 0):
+                myArray.push("yu-gi-oh");
+                break;
+            case (x % 2 == 0 && x % 3 == 0):
+                myArray.push("yu-gi");
+                break;
+            case (x % 2 == 0 && x % 5 == 0):
+                myArray.push("yu-oh");
+                break;
+            case (x % 3 == 0 && x % 5 == 0):
+                myArray.push("gi-oh");
+                break;
+            case (x % 2 == 0):
+                myArray.push('yu');
+                break;
+            case (x % 3 == 0):
+                myArray.push('gi');
+                break;
+            case (x % 5 == 0):
+                myArray.push('oh');
+                break;
+            default:
+                myArray.push(x);
+                break;
+        }
+    }
+    return myArray;
+}
+// calling myFunction.
+console.log(myFunction(100));
+console.log(myFunction(20));
+
+
+
+
+
+
 //StartNG JavaScript Task 3
 
 function divisible(number) {
@@ -36,3 +81,5 @@ function divisible(number) {
 
 const randomNumber = divisible(50)
 console.log(randomNumber)
+
+
