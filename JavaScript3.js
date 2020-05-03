1937db76a39e17486f6194fe43726468f09cff2d
ojosamuel14 <ojosamuel2014@gmail.com>
2020-05-03 23:13:15 +0100

    Update JavaScript3.js

--- JavaScript3.js
+++ JavaScript3.js
@@ -43,6 +43,44 @@ console.log(myFunction(20));
 
 
 
+
+
+
+//Second Answer
+function yuGiOhReplacer(num) {
+    let yuGiOh = [];
+    for (let i = 1; i <= num; i++) {
+        let temp = "";
+        if (i % 2 === 0) {
+            temp += "yu";
+        }
+        if (i % 3 === 0) {
+            temp += temp !== "" ? "-gi" : "gi";
+        }
+        if (i % 5 === 0) {
+            temp += temp !== "" ? "-oh" : "oh";
+        }
+        if (temp === "") {
+            temp = i;
+        }
+        yuGiOh.push(temp);
+    }
+    console.log(yuGiOh);
+    return yuGiOh;
+}
+yuGiOhReplacer(100);
+yuGiOhReplacer(45);
+
+
+
+
+
+
+
+
+
+
+//Third Answer
 //StartNG JavaScript Task 3
 
 function divisible(number) {
